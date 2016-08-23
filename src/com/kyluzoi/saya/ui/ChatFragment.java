e6d6dbe9c92124dd53df6e6413df3b69f51c120f
HappyImp <h617717344@gmail.com>
2016-08-24 00:07:40 +0800

    1.注册位数6-8 2.公告图标大小修改 3.去除视频和文件拓展

--- src/com/kyluzoi/saya/ui/ChatFragment.java
+++ src/com/kyluzoi/saya/ui/ChatFragment.java
@@ -95,11 +95,13 @@ public class ChatFragment extends EaseChatFragment implements EaseChatFragment.E
         //demo这里不覆盖基类已经注册的item,item点击listener沿用基类的
         super.registerExtendMenuItem();
         //增加扩展item
-        inputMenu.registerExtendMenuItem(R.string.attach_video, R.drawable.em_chat_video_selector, ITEM_VIDEO, extendMenuItemClickListener);
-        inputMenu.registerExtendMenuItem(R.string.attach_file, R.drawable.em_chat_file_selector, ITEM_FILE, extendMenuItemClickListener);
+        //inputMenu.registerExtendMenuItem(R.string.attach_video, R.drawable.em_chat_video_selector, ITEM_VIDEO, extendMenuItemClickListener);
+        //删除文件扩展
+        //inputMenu.registerExtendMenuItem(R.string.attach_file, R.drawable.em_chat_file_selector, ITEM_FILE, extendMenuItemClickListener);
         if(chatType == Constant.CHATTYPE_SINGLE){
             inputMenu.registerExtendMenuItem(R.string.attach_voice_call, R.drawable.em_chat_voice_call_selector, ITEM_VOICE_CALL, extendMenuItemClickListener);
-            inputMenu.registerExtendMenuItem(R.string.attach_video_call, R.drawable.em_chat_video_call_selector, ITEM_VIDEO_CALL, extendMenuItemClickListener);
+            //删除视频扩展
+            //inputMenu.registerExtendMenuItem(R.string.attach_video_call, R.drawable.em_chat_video_call_selector, ITEM_VIDEO_CALL, extendMenuItemClickListener);
         }
     }
     
